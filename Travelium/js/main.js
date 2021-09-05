a3bed6545931dbd37a012db2f8e393cf0b3e8139
Rangga Bayu <67182783+xrd027@users.noreply.github.com>
2021-09-05 16:29:55 +0700

    Add files via upload
    KP

---
+++ Travelium/js/main.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+     /*---Navbar Shrink---*/
+     $(window).on("scroll",function(){
+        if($(this).scrollTop() > 90){
+            $(".navbar").addClass("navbar-shrink");
+        }
+        else{
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    });
+
+    /*---Features Carousel---*/
+    $('.features-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+
+    /*---Discover Carousel---*/
+    $('.discover-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:4,
+            }
+        }
+    });
+
+    /*---Testimonials Carousel---*/
+    $('.testimonials-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+
+     /*---Team Carousel---*/
+     $('.team-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+
+    /*---Scrollit---*/
+    $.scrollIt({
+        topOffset: -50
+    });
+
+});
